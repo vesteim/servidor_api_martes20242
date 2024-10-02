b89bcc0ae4d0551ef22b7f1428ffce55a02b12e9
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2024-10-01 20:44:41 -0500

    f01/10/2024 se construyes nuestros servicios y los endpoints

--- app/api/routes/endpoints.py
+++ app/api/routes/endpoints.py
@@ -17,4 +17,39 @@ def conectarConBd():
         raise error #cuenta que paso (el error)
 
     finally:
-        baseDatos.close()  #cerrar peticion    
+        baseDatos.close()  #cerrar peticion  
+
+    
+#   construyendo nuestros servicios
+
+#Cada servicio (operacion o transaccion en BD) debe programarse como una funcion
+@rutas.post("/usuario", response_model=Usuario, summary="Registrar un usuario en la base de datos") #documentando un servicio 
+def guardarUsuario(datosUsuario:UsuarioDTOPeticion,database:Session=Depends(conectarConBd)): # con esto podemos comunicarme con la base de datos
+    # debemos filtrar los datos, para que coincidan con la base de datos
+    try:
+        usuario=Usuario(
+            nombres=datosUsuario.nombres,
+            fechaNacimiento=datosUsuario.fechaNAcimiento,
+
+        )
+        #ordenando a la base de datos
+        database.add(usuario) #agregemelo
+        database.commit()     #tomele foto
+        database.refresh(usuario) #refresquelo
+        return usuario  #devuelvamelo
+
+    except Exception as error:
+        database.rollback()
+        raise HTTPException(status_code=400, detail="Tenemos un problema {error}")
+@rutas.post("/usuario",response_model=list[UsuarioDTOEwspuesta],summary="Buscar todos los usuarios en BD")
+def buscarUsuarios():
+    try:
+        usuarios=database.query(Usuario).all()
+        return usuarios
+
+    except Exception as error:
+        database.rollback()
+        raise HTTPException(status_code=400, detail="Tenemos un problema {error}")
+
+
+#Tarea hacer Gasto
